d54f9c57ec3c18b7093c3a0d49d95e6e2c5912dd
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-03-04 13:17:36 +0900

    탐욕 알고리즘 부분 배낭 문제

--- src/GreedyAlgorithm/FractionalKnapsackProblem.java
+++ src/GreedyAlgorithm/FractionalKnapsackProblem.java
@@ -1,5 +1,43 @@
 package GreedyAlgorithm;
 
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class FractionalKnapsackProblem {
-    Integer[][] objectList = {{10, 10}, {15, 12}, {20, 10}, {25, 8}, {30, 5}};
+
+
+    public void knapsackFunc(Integer[][] objectList, double capacity) {
+        double totalValue = 0.0;
+        double fraction = 0.0;
+
+        Arrays.sort(objectList, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+                return (o2[1] / o2[0]) - (o1[1] / o1[0]);
+            }
+        });
+
+        for (int index = 0; index < objectList.length; index++) {
+            if ((capacity - (double)objectList[index][0]) > 0) {
+                capacity -= (double)objectList[index][0];
+                totalValue += (double)objectList[index][1];
+                System.out.println("무게: " + objectList[index][0] + ", 가치: " + objectList[index][1]);
+            } else {
+                fraction = capacity / (double) objectList[index][0];
+                totalValue += (double) objectList[index][1] * fraction;
+                System.out.println("무게: " + objectList[index][0] + ", 가치: " + objectList[index][1] + ", 비율: " + fraction);
+                break;
+            }
+        }
+        System.out.println("총 담을 수 있는 가치: " + totalValue);
+
+    }
+
+    public static void main(String[] args) {
+        FractionalKnapsackProblem problem = new FractionalKnapsackProblem();
+        Integer[][] objectList = {{10, 10}, {15, 12}, {20, 10}, {25, 8}, {30, 5}};
+        problem.knapsackFunc(objectList, 30.0);
+
+    }
 }
