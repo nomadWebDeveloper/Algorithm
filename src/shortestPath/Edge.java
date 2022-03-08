22b484c6359d524f407392ac2036669f810ee2b5
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-03-08 13:00:18 +0900

    다익스트라 알고리즘

--- src/shortestPath/Edge.java
+++ src/shortestPath/Edge.java
@@ -2,8 +2,11 @@ package shortestPath;
 
 public class Edge implements Comparable<Edge> {
 
+
     public int distance;
     public String vertex;
+
+    // 노드 생성자
     public Edge(int distance, String vertex) {
         this.distance = distance;
         this.vertex = vertex;
@@ -12,6 +15,8 @@ public class Edge implements Comparable<Edge> {
     public String toString() {
         return "vertex: " + this.vertex + ", distance: " + this.distance;
     }
+
+    // Comparable을 이용해 노드의 내부 순서를 설정한다.
     @Override
     public int compareTo(Edge edge) {
         return this.distance - edge.distance;
