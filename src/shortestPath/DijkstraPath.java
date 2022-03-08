e589e71ec6d6c2074eb6bc2ae1730b1a60722864
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-03-08 20:40:58 +0900

    다익스트라 알고리즘

--- src/shortestPath/DijkstraPath.java
+++ src/shortestPath/DijkstraPath.java
@@ -10,31 +10,51 @@ public class DijkstraPath {
         String currentNode, adjacent;
         ArrayList<Edge> nodeList;
         HashMap<String, Integer> distances = new HashMap<>();
+
+        // 노드 배열의 거리값에 최댓값 넣기
         for(String key: graph.keySet()) {
             distances.put(key, Integer.MAX_VALUE);
         }
+
+        // 출발 노드 설정
         distances.put(start, 0);
 
+
+        // 우선순위 큐 생성
         PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
+
+        // 우선순위 큐에 없데이트 된 출발 노드 값 넣기
         priorityQueue.add(new Edge(distances.get(start), start));
 
         while(priorityQueue.size() > 0) {
+
+            // 우선순위 큐에서 추출
             edgeNode = priorityQueue.poll();
+
+            // 추출 된 노드의 정보
             currentDistance = edgeNode.distance;
             currentNode = edgeNode.vertex;
 
+            // 배열에 저장 되어 있는 거리 값이 더 작을 때
             if(distances.get(currentNode) < currentDistance) {
                 continue;
             }
 
+            // 노드 A에 인접한 노드들 추출
             nodeList = graph.get(currentNode);
             for(int index = 0; index < nodeList.size(); index++) {
                 adjacentNode = nodeList.get(index);
                 adjacent = adjacentNode.vertex;
                 weight = adjacentNode.distance;
+
+                // 큐에서 추출된 노드의 거리와 추출된 노드와 인접한 노드의 거리의 합 = 인접한 노드까지 이동한 거리
                 distance = currentDistance + weight;
 
+
+                // 배열에 저장된 거리보다 인접한 노드까지 이동한 거리가 짧을 때
                 if (distance < distances.get(adjacent)) {
+
+                    // 배열 업데이트 한 뒤 우선순위 큐에 넣기
                     distances.put(adjacent, distance);
                     priorityQueue.add(new Edge(distance, adjacent));
                 }
--- src/shortestPath/DijkstraPathTest.java
+++ src/shortestPath/DijkstraPathTest.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 
 public class DijkstraPathTest {
     public static void main(String[] args) {
+
+        // 그래프 생성
         HashMap<String, ArrayList<Edge>> graph = new HashMap<>();
         graph.put("A", new ArrayList<>(Arrays.asList(new Edge(8, "B"), new Edge(1, "C"), new Edge(2, "D"))));
         graph.put("B", new ArrayList<>());
@@ -14,7 +16,10 @@ public class DijkstraPathTest {
         graph.put("E", new ArrayList<>(Arrays.asList(new Edge(1, "F"))));
         graph.put("F", new ArrayList<>(Arrays.asList(new Edge(5, "A"))));
 
+
         DijkstraPath dObject = new DijkstraPath();
+
+        // 출발 노드가 A인 다익스트라 알고리즘
         System.out.println(dObject.dijkstraFunc(graph, "A"));
     }
 }
