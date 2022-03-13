72588ce8615f6cc072cd574d14801bbb23ab147b
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-03-13 19:41:43 +0900

    크루스칼 알고리즘

---
+++ src/kruskalsAlgorithm/Graph.java
@@ -0,0 +1,35 @@
+package kruskalsAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Graph {
+
+    public void generate() {
+        ArrayList<String> vertices = new ArrayList<String>(Arrays.asList("A", "B", "C", "D", "E", "F", "G"));
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+        edges.add(new Edge(7, "A", "B"));
+        edges.add(new Edge(5, "A", "D"));
+        edges.add(new Edge(7, "B", "A"));
+        edges.add(new Edge(8, "B", "C"));
+        edges.add(new Edge(9, "B", "D"));
+        edges.add(new Edge(7, "B", "E"));
+        edges.add(new Edge(8, "C", "B"));
+        edges.add(new Edge(5, "C", "E"));
+        edges.add(new Edge(5, "D", "A"));
+        edges.add(new Edge(9, "D", "B"));
+        edges.add(new Edge(7, "D", "E"));
+        edges.add(new Edge(6, "D", "F"));
+        edges.add(new Edge(7, "E", "B"));
+        edges.add(new Edge(5, "E", "C"));
+        edges.add(new Edge(7, "E", "D"));
+        edges.add(new Edge(8, "E", "F"));
+        edges.add(new Edge(9, "E", "G"));
+        edges.add(new Edge(6, "F", "D"));
+        edges.add(new Edge(8, "F", "E"));
+        edges.add(new Edge(11, "F", "G"));
+        edges.add(new Edge(9, "G", "E"));
+        edges.add(new Edge(11, "G", "F"));
+    }
+
+}
---
+++ src/kruskalsAlgorithm/KruskalPath.java
@@ -0,0 +1,72 @@
+package kruskalsAlgorithm;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class KruskalPath {
+
+    HashMap<String, String> parent = new HashMap<>();
+    HashMap<String, Integer> rank = new HashMap<>();
+
+    public String find (String node) {
+
+        //path compression
+        if (parent.get(node) != node) {
+            parent.put(node, find(parent.get(node)));
+
+        }
+        return parent.get(node);
+    }
+
+    public void union (String nodeV, String nodeU) {
+        String root1 = find(nodeV);
+        String root2 = find(nodeU);
+
+        //union - by - rank
+        if (rank.get(root1) > rank.get(root2)) {
+            parent.put(root2, root1);
+        }
+        else {
+            parent.put(root1, root2);
+            if (rank.get(root1) == rank.get(root2)) {
+                rank.put(root2, rank.get(root2) + 1);
+            }
+        }
+    }
+
+    public void makeSet(String node) {
+        parent.put(node, node);
+        rank.put(node, 0);
+    }
+
+    public ArrayList<Edge> kruskalFunc(ArrayList<String> vertices, ArrayList<Edge> edges) {
+        ArrayList<Edge> mst = new ArrayList<>();
+        Edge currentEdge;
+        int weight;
+        String nodeV, nodeU;
+
+        //1.initialize
+
+        for(int index = 0; index < vertices.size(); index++) {
+            makeSet(vertices.get(index));
+        }
+
+        //2.sorting
+
+        Collections.sort(edges);
+
+        for (int index = 0; index < edges.size(); index++) {
+            currentEdge = edges.get(index);
+            if (find(currentEdge.nodeV) != find(currentEdge.nodeU)) {
+                union(currentEdge.nodeV, currentEdge.nodeU);
+                mst.add(currentEdge);
+            }
+        }
+
+        return mst;
+    }
+
+
+}
